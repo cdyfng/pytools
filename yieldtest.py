9206c9536eb15ab73bf39005c9d21c4173c4d985
cdyfng <xiaofeng_chen2007@163.com>
2014-09-26 12:11:22 +0800

    yieldtest.py add

---
+++ yieldtest.py
@@ -0,0 +1,15 @@
+def yield_test():
+    yield 1
+    yield 2
+    yield 3
+
+
+def read_file(fpath): 
+    BLOCK_SIZE = 1024 
+    with open(fpath, 'rb') as f: 
+        while True: 
+            block = f.read(BLOCK_SIZE) 
+            if block: 
+                yield block 
+            else: 
+                return
